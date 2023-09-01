c11ca9f47659735d07b4052d8a85ef49947f12d9
samsontands <139669515+samsontands@users.noreply.github.com>
2023-09-01 11:15:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-import openai
+ import openai
 import streamlit as st
-import streamlit_authenticator as stauth
-import yaml
-from yaml.loader import SafeLoader
 
 # Custom message function to create chat-like appearance
 def message(content, is_user=False, key=None):
@@ -18,21 +15,16 @@ openai.api_key = st.secrets["OPENAI_API_KEY"]
 # Define the Fine-Tuned Model ID from secrets
 fine_tuned_model_id = st.secrets["FINE_TUNED_MODEL_ID"]
 
-# Usernames and Passwords from Streamlit secrets
-usernames = st.secrets["USERNAMES"]
-hashed_passwords = st.secrets["PASSWORDS"]
+# Sidebar - let user choose model
+model_name = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4", "Fine-Tuned Model"))
 
-# Create the authenticator object
-authenticator = stauth.Authenticate(
-    usernames,
-    "random_cookie_name",
-    "random_signature_key",
-    30,
-    []
-)
-
-# Render the login widget
-name, authentication_status, username = authenticator.login('Login', 'main')
+# Map model names to OpenAI model IDs
+if model_name == "GPT-3.5":
+    model = "gpt-3.5-turbo"
+elif model_name == "GPT-4":
+    model = "gpt-4"
+else:
+    model = fine_tuned_model_id
 
 # Initialize session state variables
 if 'generated' not in st.session_state:
@@ -52,31 +44,49 @@ if 'total_tokens' not in st.session_state:
 if 'total_cost' not in st.session_state:
     st.session_state['total_cost'] = 0.0
 
+# Generate a response
+def generate_response(prompt):
+    st.session_state['messages'].append({"role": "user", "content": prompt})
+    completion = openai.ChatCompletion.create(
+        model=model,
+        messages=st.session_state['messages']
+    )
+    response = completion.choices[0].message.content
+    st.session_state['messages'].append({"role": "assistant", "content": response})
+    total_tokens = completion.usage.total_tokens
+    prompt_tokens = completion.usage.prompt_tokens
+    completion_tokens = completion.usage.completion_tokens
+    return response, total_tokens, prompt_tokens, completion_tokens
+
 # Main app logic
 st.title("SAMGPT - Your Neighborhood Bot 😬")
 
-if authentication_status:
-    authenticator.logout('Logout', 'main')
-    st.write(f'Welcome *{name}*')
+# Text input and button
+with st.form(key='my_form', clear_on_submit=True):
+    user_input = st.text_area("You:", key='input', height=100)
+    submit_button = st.form_submit_button(label='Send')
 
-    # Text input and button
-    with st.form(key='my_form', clear_on_submit=True):
-        user_input = st.text_area("You:", key='input', height=100)
-        submit_button = st.form_submit_button(label='Send')
+if submit_button and user_input:
+    output, total_tokens, prompt_tokens, completion_tokens = generate_response(user_input)
+    st.session_state['past'].append(user_input)
+    st.session_state['generated'].append(output)
+    st.session_state['model_name'].append(model_name)
+    st.session_state['total_tokens'].append(total_tokens)
 
-    if submit_button and user_input:
-        pass  # Generate a response (your existing code here)
+    # Calculate cost
+    if model_name == "GPT-3.5":
+        cost = total_tokens * 0.002 / 1000
+    else:
+        cost = (prompt_tokens * 0.03 + completion_tokens * 0.06) / 1000
 
-    # Display chat history
-    for i in range(len(st.session_state['generated'])):
-        message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
-        message(st.session_state["generated"][i], key=str(i))
+    st.session_state['cost'].append(cost)
+    st.session_state['total_cost'] += cost
 
-    # Display total cost
-    st.sidebar.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+# Display chat history
+for i in range(len(st.session_state['generated'])):
+    message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+    message(st.session_state["generated"][i], key=str(i))
+    st.write(f"Model used: {st.session_state['model_name'][i]}; Number of tokens: {st.session_state['total_tokens'][i]}; Cost: ${st.session_state['cost'][i]:.5f}")
 
-elif authentication_status == False:
-    st.error('Username/password is incorrect')
-
-elif authentication_status == None:
-    st.warning('Please enter your username and password')
+# Display total cost
+st.sidebar.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
